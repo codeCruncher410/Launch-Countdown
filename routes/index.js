464ef5bdde62a0962ada1b4ecbeb63d4faaafc19
codeCruncher410 <richards.lucas2@gmail.com>
2023-06-01 06:53:20 -0400

    Putting in the countdown timer in milliseconds.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const launchTime = new Date('12/29/2023 10:00').getTime()
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Countdown' });
+  const currentTime = new Date().getTime()
+  const numberOfMilliseconds = parseInt(launchTime - currentTime)
+  res.render(
+    'index', 
+    { title: 'Time until Launch', countDown: numberOfMilliseconds });
 });
 
 module.exports = router;
